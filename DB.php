b09a088303eb7f8a02a6eb2f86bf31499f75b468
Muizz Zafar <muizafar@Muizzs-MacBook-Air.local>
2021-01-17 01:46:20 -0500

    premature search algo implemented

--- DB.php
+++ DB.php
@@ -5,13 +5,14 @@ require_once './vendor/autoload.php';
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 
-// Recipe {Ingredients}
+// make ingredients a list
 
 class Rtdb {
     protected $database;
     protected $dbname = 'recipe';
-    protected $ingredient = 'Ingredient';
-    protected $instructions = 'How-to-make';
+    protected $ingredientKey = 'Ingredient';
+    protected $instructionsKey = 'How-to-make';
+    static $searchResult = [];
     
     public function __construct(){
         $factory = (new Factory)->withServiceAccount(__DIR__.'/hackathon-rtdb-firebase-adminsdk-sbm0f-0382ae8bc4.json');
@@ -27,15 +28,20 @@ class Rtdb {
         return $this->database->getReference()->getChild($this->dbname)->getValue();
     }
 
+    // Gets the name of all the recipes in the db
     public function getAllRecipeName () {
         $allRecipe = $this->getAllRecipe();
         return array_keys($allRecipe);
     }
 
+    // Gets 10 recipes from the startIndex from the db
+    public function getRecipeRange(int $startIndex = NULL) {
+        $recipeArr = $this->getAllRecipe();
+        return array_slice($recipeArr, $startIndex, 10);
 
+    }
 
     // Gets a specific recipe and all info of that recipe from the db
-    // Id of the recipe
     public function getRecipe(string $recipeId = NULL){
         if (empty($recipeId) || !isset($recipeId)) { return FALSE; }
 
@@ -54,13 +60,26 @@ class Rtdb {
         foreach ($data as $key => $value){
             $this->database->getReference()->getChild($this->dbname)->getChild($key)->set($value);
         }
-
-        return TRUE;
-        
+        return TRUE; 
     }
 
-    public function search(array $ingredients) {
-        $allRecipe = getAllRecipeName();
+    public function search($ingredientsArr) {
+        $allRecipe = $this->getAllRecipe();
+        $tempArr = [];
+        
+        foreach($ingredientsArr as $ingredient) {
+            foreach ($allRecipe as $recipe) {
+                // print_r($recipe[$this->instructions]);
+                // If any of the ingredients match, at it to the list
+                if(str_contains($recipe[$this->ingredientKey], $ingredient)) {
+                    array_push($tempArr, $recipe);
+                    array_push($this->$searchResult, $recipe);
+                    break;
+                }
+            }
+        }
+
+        return $tempArr;
 
     }
 
@@ -75,15 +94,25 @@ class Rtdb {
 
 $recipe = new Rtdb();
 
+    // $recipe->search();
+
     // print_r($recipe->getAllRecipeName());
 
     // print_r($recipe->getRecipes('Cake'));
 
-    var_dump($recipe->insert([
-    'Cake' => 'This is the recipe for cake',
-    'Rice' => 'This is the recipe for rice',
-    'Donuts' => 'This is the recipe for donuts',
-    'Burger' => ['Ingredients' => '1. Milk 2. Sugar',
-                 'How-to-make' => '1. Add Milk 2. Add Sugar'],
-    ]));
+    // Writing to the db
+    // var_dump($recipe->insert([
+    // 'Cake' => ['Ingredients' => ['milk' => True, 
+    //                              'meat' => True],
+    //             'How-to-make' => 'Cake'],
+
+    // 'Rice' => ['Ingredients' => '1. Milk 2. Sugar',
+    //              'How-to-make' => 'Rice'],
+
+    // 'Donut' => ['Ingredients' => '1. Milk 2. Sugar',
+    //               'How-to-make' => 'Donut'],
+
+    // 'Burger' => ['Ingredients' => '1. Milk 2. Sugar',
+    //              'How-to-make' => 'Burger'],
+    // ]));
 
