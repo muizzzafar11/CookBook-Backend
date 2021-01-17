226b96e74e84b51acb1b06d14b9e984fcc0ee37a
Muizz Zafar <muizafar@Muizzs-MacBook-Air.local>
2021-01-17 03:21:31 -0500

    search algorithm with pagination working

--- DB.php
+++ DB.php
@@ -10,9 +10,11 @@ use Kreait\Firebase\ServiceAccount;
 class Rtdb {
     protected $database;
     protected $dbname = 'recipe';
-    protected $ingredientKey = 'Ingredient';
+    protected $ingredientKey = 'Ingredients';
     protected $instructionsKey = 'How-to-make';
     static $searchResult = [];
+    
+    // Get the list from the front-end
     
     public function __construct(){
         $factory = (new Factory)->withServiceAccount(__DIR__.'/hackathon-rtdb-firebase-adminsdk-sbm0f-0382ae8bc4.json');
@@ -23,7 +25,7 @@ class Rtdb {
         
     }
 
-    // Gets all the recipeies from the db
+    // Gets all the recipes from the db
     public function getAllRecipe () {
         return $this->database->getReference()->getChild($this->dbname)->getValue();
     }
@@ -35,9 +37,18 @@ class Rtdb {
     }
 
     // Gets 10 recipes from the startIndex from the db
-    public function getRecipeRange(int $startIndex = NULL) {
-        $recipeArr = $this->getAllRecipe();
-        return array_slice($recipeArr, $startIndex, 10);
+    public function getRecipeRange(int $startIndex = NULL, $ingredients = []) {
+        
+        // Run the search algorithm the first time to populate the searchResult Array
+        // if((count(self::$searchResult) == 0) && (count($ingredients) > 0)) {
+        //     $this->search();
+        // }
+        $this->search($ingredients);
+        if(count(self::$searchResult) >= 10){
+            return array_slice(self::$searchResult, $startIndex, 9);
+        } else {
+            return self::$searchResult;
+        }
 
     }
 
@@ -63,24 +74,23 @@ class Rtdb {
         return TRUE; 
     }
 
-    public function search($ingredientsArr) {
+    public function search($ingredientsArr = []) {
         $allRecipe = $this->getAllRecipe();
         $tempArr = [];
         
         foreach($ingredientsArr as $ingredient) {
             foreach ($allRecipe as $recipe) {
-                // print_r($recipe[$this->instructions]);
-                // If any of the ingredients match, at it to the list
+
+                // If any of the ingredients match, add it to the list
                 if(str_contains($recipe[$this->ingredientKey], $ingredient)) {
                     array_push($tempArr, $recipe);
-                    array_push($this->$searchResult, $recipe);
-                    break;
                 }
+                // print_r($recipe[$this->ingredientKey]);
             }
         }
-
-        return $tempArr;
-
+        self::$searchResult = $tempArr;
+        // print_r(self::$searchResult);
+        return TRUE;
     }
 
     // Don't think we need this
@@ -94,7 +104,9 @@ class Rtdb {
 
 $recipe = new Rtdb();
 
-    // $recipe->search();
+    // print_r($recipe->getRecipeRange(0,['Spice']));
+
+    // $recipe->search(['Spice']);
 
     // print_r($recipe->getAllRecipeName());
 
@@ -102,17 +114,44 @@ $recipe = new Rtdb();
 
     // Writing to the db
     // var_dump($recipe->insert([
-    // 'Cake' => ['Ingredients' => ['milk' => True, 
-    //                              'meat' => True],
-    //             'How-to-make' => 'Cake'],
 
-    // 'Rice' => ['Ingredients' => '1. Milk 2. Sugar',
+    //             'Rice' => ['Ingredients' => '1. Milk 2. Sugar',
     //              'How-to-make' => 'Rice'],
 
-    // 'Donut' => ['Ingredients' => '1. Milk 2. Sugar',
+    //             'Donut' => ['Ingredients' => '1. Milk 2. Sugar Spice',
     //               'How-to-make' => 'Donut'],
 
-    // 'Burger' => ['Ingredients' => '1. Milk 2. Sugar',
+    //             'Burger' => ['Ingredients' => '1. Milk 2. Sugar',
     //              'How-to-make' => 'Burger'],
+    
+    //              'Test1' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test1'],
+
+
+    //              'Test2' => ['Ingredients' => '1. Milk 2. Sugar',
+    //              'How-to-make' => 'Test2'],
+
+
+    //              'Test3' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test3'],
+
+    //              'Test4' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test4'],
+
+    //              'Test5' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test5'],
+
+    //              'Test6' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test6'],
+
+    //              'Test7' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test7'],
+
+    //              'Test8' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test8'],
+
+    //              'Test9' => ['Ingredients' => '1. Milk 2. Sugar Spice',
+    //              'How-to-make' => 'Test9'],
+
     // ]));
 
